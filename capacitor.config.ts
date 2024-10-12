80ef5bc4c1866c9c94fbda8841dd19f27a5ac458
sohithvishnu <sohithvishnusai@icloud.com>
2024-10-12 16:30:57 +0200

    Added the TO-DO-Code and Added a Testing Grounds

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+	appId: 'com.helloworld.app',
+	appName: 'helloworld',
+	webDir: 'build'
+};
+
+export default config;
