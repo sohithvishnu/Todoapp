c4d9098438ea3b32a3137075c827c47fd1b0d761
sohithvishnu <sohithvishnusai@icloud.com>
2024-11-04 01:03:18 +0100

    To-do app basic working

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,16 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
+    kit: {
+        adapter: adapter({
+            pages: 'build', // The directory to output the HTML files
+            assets: 'build', // The directory to output static assets
+            fallback: 'index.html' // No fallback for SPA routing
+        })
+    },
+    preprocess: vitePreprocess()
 };
 
 export default config;
